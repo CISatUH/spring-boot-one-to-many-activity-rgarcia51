28e0f3793e086f276986c14b63211035a28a3759
Rubi Garcia <rgarcia51@uh.edu>
2019-02-27 00:31:57 -0600

    START OVER

---
+++ src/main/resources/db/migration/V20190227002854__add_table.sql
@@ -0,0 +1,18 @@
+create table TARGET.PUBLIC.customer
+(
+  customer_id serial      not null
+    constraint customer_pkey primary key,
+  last_name   varchar(20) not null,
+  first_name  varchar(20) not null
+);
+
+create table TARGET.public.address
+(
+  address_id    serial primary key,
+  street_name   varchar(20),
+  street_number varchar(10),
+  state         varchar(20),
+  zipcode       int,
+  custumer_id   integer references customer (customer_id)
+
+);
